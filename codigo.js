6da3565efa7b4e7970032d84012c4e414f670bf0
jared-animas <129801788+jared-animas@users.noreply.github.com>
2023-05-14 22:08:51 -0500

    cambio1

--- codigo.js
+++ codigo.js
@@ -1,4 +1,3 @@
-
 var TextoEntrada = document.getElementById('Entrada');
 var TextoSalida = document.getElementById('Salida');
 const botonCodificar = document.getElementById("Codificar");
@@ -6,22 +5,22 @@ const botonDecodificar = document.getElementById("Decodificar");
 const borrar = document.getElementById("Limpiar");
 const copiar = document.getElementById("Copiar");
 const pegar = document.getElementById("Pegar");
-var texto2 = [];
+var texto2;
 var resultado = "";
+let letras = ["a","e","i","o","u"];
+let cambio = ["ai","enter","imes","ober","ufat"];
 
 function mostrar() { //muestra el Textarea donde se imprime el texto codificado/decodificado y muestra el boton copiar
     document.getElementById("Sin-Texto").style.display = "none";
     document.getElementById("Copiar").style.display = "initial";
     document.getElementById("Salida").style.display = "initial";
 }
-  
 
 copiar.addEventListener("click", () => {    //boton para copiar texto
     // el método select es utilizado para seleccionar el contenido de un campo de texto
     TextoSalida.select();
         // el método document.execCommand("copy") copia el texto seleccionado al portapapeles
         let exito = document.execCommand("copy");
-        console.log('funca');
 });
  
 pegar.addEventListener("click", () => {
@@ -55,35 +54,20 @@ borrar.addEventListener("click",function(){      // boton para borrar textos
     document.getElementById("Sin-Texto").style.display = "initial";
     document.getElementById("Salida").style.display = "none";
     document.getElementById("Copiar").style.display = "none";
+    texto2 = "";
 });
 
 //proceso para codificar texto
 function CodificarTexto(texto) {
-
+    texto2 = [];
+    //remplazar el contenido del texto 
     for (let index = 0; index < TextoEntrada.value.length; index++) {
-
-         //condicional para ignorarletras que no sean del alfabeto
-        if (TextoEntrada[index] != "a" && TextoEntrada[index] != "e"   
-        && TextoEntrada[index] != "i" && texto[index] != "o" 
-        && texto[index]!= "u" ) {
-            texto2[index] = texto[index];
-        }
-
+        texto2[index] = texto[index]; // ingresa letra por letra del texto al array
         //condicional para modificar las letras del alfabeto
-        if (TextoEntrada.value[index]=="a") {
-           texto2[index] = "ai";    
-        }
-        if (TextoEntrada.value[index]=="e") {
-           texto2[index] = "enter";
-        }
-        if (TextoEntrada.value[index]=="i") {
-           texto2[index] = "imes";
-        }
-        if (TextoEntrada.value[index]=="o") {
-          texto2[index] = "ober";
-        }
-        if (TextoEntrada.value[index]=="u") {
-         texto2[index] = "ufat";
+        for (let i = 0; i <= letras.length; i++) {
+            if (TextoEntrada.value[index]==letras[i]) {
+                texto2[index] = cambio[i];
+             }
         }
     }
     resultado = texto2.toString();      //convertir array a cadena de texto
@@ -93,11 +77,7 @@ function CodificarTexto(texto) {
 //proceso para decodificar texto
 function DecodificarTexto(texto) {
     resultado = TextoEntrada.value.toString();  //covertir el texto de entrada en una cadena de texto
-    //funcion para remplazar caracteres
-    resultado = resultado.replaceAll("ai","a");
-    resultado = resultado.replaceAll("enter","e");
-    resultado = resultado.replaceAll("imes","i");
-    resultado = resultado.replaceAll("ufat","u");
-    resultado = resultado.replaceAll("ober","o");
+    for (var i = 0; i < letras.length; i++){
+        resultado = resultado.replaceAll(cambio[i],letras[i]);
+    }
 }
-
