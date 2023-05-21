8c7bc65af4944296da54a83d2940a107dbb96017
jared-animas <129801788+jared-animas@users.noreply.github.com>
2023-05-20 21:37:51 -0500

    v2.1

--- codigo.js
+++ codigo.js
@@ -71,3 +71,14 @@ function DecodificarTexto(texto) {// proceso para decodificar texto
   }
   return texto;
 }
+
+TextoEntrada.addEventListener('keydown', autosize);// autosize de textbox
+function autosize(){
+  var el = this;
+  setTimeout(function(){
+    el.style.cssText = 'height:auto; padding:0';
+    // for box-sizing other than "content-box" use:
+    // el.style.cssText = '-moz-box-sizing:content-box';
+    el.style.cssText = 'height:' + el.scrollHeight + 'px';
+  },0);
+}
